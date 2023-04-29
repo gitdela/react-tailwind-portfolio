c4a28117575b5895018069d27bd3d175c2ab0020
gitdela <scriptingdela@gmail.com>
2023-04-29 01:43:54 +0000

    Set up files & add NavBar component

---
+++ re-tw-portfolio/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+const NavBar = () => {
+  return (
+    <div className='bg-black flex justify-between items-center w-full h-20 text-white fixed'>
+      <h1>KENNARTEY</h1>
+    </div>
+  );
+};
+
+export default NavBar;
